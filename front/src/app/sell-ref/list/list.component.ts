677f1222d1668836f81d260f812d0f4f5605e251
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-12-07 13:49:21 +0100

    grid started

--- front/src/app/sell-ref/list/list.component.ts
+++ front/src/app/sell-ref/list/list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ReferenceService } from 'src/app/services/reference.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  constructor(public reference: ReferenceService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
