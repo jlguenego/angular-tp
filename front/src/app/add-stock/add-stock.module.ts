bde6ac1d1c3393b51a85527e47e6dd76315dde36
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-12-07 13:12:35 +0100

    form html and ts

--- front/src/app/add-stock/add-stock.module.ts
+++ front/src/app/add-stock/add-stock.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AddStockRoutingModule } from './add-stock-routing.module';
 import { FormComponent } from './form/form.component';
 import { SuccessComponent } from './success/success.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [FormComponent, SuccessComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AddStockRoutingModule
   ]
 })
--- front/src/app/add-stock/form/form.component.ts
+++ front/src/app/add-stock/form/form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  f = new FormGroup({
+    label: new FormControl('Tournevis', Validators.required),
+    category: new FormControl('Outils', Validators.required),
+    quantity: new FormControl(34, Validators.required),
+    price: new FormControl(3.2),
+  });
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  submit() {
+    this.router.navigateByUrl('/add-stock/success');
   }
-
 }
